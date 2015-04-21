5161031865e072e12759fccdf67f6eda117adb50
yahavi <yahav.itzhak@mail.huji.ac.il>
2015-04-21 20:47:26 +0300

    Initial commit

---
+++ app/src/androidTest/java/huji/ac/il/stick_defence/ApplicationTest.java
@@ -0,0 +1,13 @@
+package huji.ac.il.stick_defence;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
