6e2115b3b74d01713b9fbec9c8651167fe8a7cdb
Yahav <yahav.itzhak@mail.huji.ac.il>
2015-08-14 16:28:12 +0300

    Clean up3

--- app/src/main/java/huji/ac/il/stick_defence/MainMenu.java
+++ app/src/main/java/huji/ac/il/stick_defence/MainMenu.java
@@ -80,8 +80,6 @@ public class MainMenu extends Activity implements DoProtocolAction {
                 Button decreasePlayersButton = (Button) findViewById(R.id.less_players);
                 Button startButton = (Button) findViewById(R.id.start);
                 final TextView nPlayersText = (TextView) findViewById(R.id.num_of_players);
-                RadioGroup networkChoice = (RadioGroup) findViewById(R.id.network_choice);
-                networkChoice.check(R.id.wifi);
                 nPlayersText.setText(String.valueOf(nPlayers[0]));
 
                 increasePlayersButton.setOnClickListener(new View.OnClickListener() {
